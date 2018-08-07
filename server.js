2375165ba1bcf318382446780cf47701992f73f8
Victoria Gonzalez <vgonzalez3092@gmail.com>
2018-08-07 19:54:20 -0400

    minor changes

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ var good = ["goodOne", "goodTwo", "goodThree"];
 var bad = ["badOne", "badTwo", "badThree"];
 
 function handleRequest(request, response){
-    response.end("It works!! Path hit: " + request.url);
+    response.end(good[Math.floor(Math.random() * good.length)]);
 }
 var server = http.createServer(handleRequest);
 server.listen(PORT, function(){
-    console.log(good[Math.floor(Math.random() * good.length)] + "Server listening on: http://localhost: " + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
 
 
 
 function handleRequestTwo(request, response){
-    response.end("It works!! Path hit: " + request.url);
+    response.end(bad[Math.floor(Math.random() * bad.length)]);
 }
 var serverTwo = http.createServer(handleRequest);
 serverTwo.listen(PORT2, function(){
-    console.log(bad[Math.floor(Math.random() * bad.length)] + "Server listening on: http://localhost: " + PORT2);
+    console.log("Server listening on: http://localhost:" + PORT2);
 });
