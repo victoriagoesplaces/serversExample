784e62538d43c48745b300ba0031713916fc70ec
Victoria Gonzalez <vgonzalez3092@gmail.com>
2018-08-07 19:45:09 -0400

    server.js activity

---
+++ server.js
@@ -0,0 +1,23 @@
+var http = require("http");
+var PORT = 7000;
+var PORT2 = 7500;
+var good = ["goodOne", "goodTwo", "goodThree"];
+var bad = ["badOne", "badTwo", "badThree"];
+
+function handleRequest(request, response){
+    response.end("It works!! Path hit: " + request.url);
+}
+var server = http.createServer(handleRequest);
+server.listen(PORT, function(){
+    console.log(good[Math.floor(Math.random() * good.length)] + PORT);
+});
+
+
+
+function handleRequestTwo(request, response){
+    response.end("It works!! Path hit: " + request.url);
+}
+var serverTwo = http.createServer(handleRequest);
+serverTwo.listen(PORT2, function(){
+    console.log(bad[Math.floor(Math.random() * bad.length)] + PORT);
+});
